193242e31e370d1f2e1b28018f68c976555624b7
Aakash1049 <80270676+Aakash1049@users.noreply.github.com>
2022-12-19 11:44:27 +0530

    Update index.js

--- index.js
+++ index.js
@@ -4,12 +4,12 @@ const app= require("./app")
 const connectDatabase  = require("./config/db")
 connectDatabase();
 
-// if (process.env.NODE_ENV === 'production') {
-//     //*Set static folder
-//     app.use(express.static('client/build'));
+if (process.env.NODE_ENV === 'production') {
+    //*Set static folder
+    app.use(express.static('client/build'));
     
-//     app.get('*', (req,res) => res.sendFile(path.resolve(__dirname, 'client', 'build','index.html')));
-//   }
+    app.get('*', (req,res) => res.sendFile(path.resolve(__dirname, 'client', 'build','index.html')));
+  }
 app.listen(PORT,()=>{
     console.log("server is running at port ",PORT)
 })
