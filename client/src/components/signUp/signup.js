b4d3cad4571addb4422be3477d27b8e94a073d50
Aakash Sangapure <aakashdadasangapure@gmail.com>
2023-01-20 21:18:40 +0530

    added style

--- client/src/components/signUp/signup.js
+++ client/src/components/signUp/signup.js
@@ -1,48 +1,55 @@
 import React, { useState } from 'react'
-import { Link,useNavigate } from 'react-router-dom'
+import { Link, useNavigate } from 'react-router-dom'
 import "./signUp.css"
 import M from "materialize-css"
+
 const Signup = () => {
-    const navigate = useNavigate();
-    const [name,setName]=useState("")
-    const [email,setEmail]=useState("")
-    const [password,setPassword]=useState("")
-    const postData=()=>{
-        fetch("/register",{
-            method:"POST",
-            headers:{
-                "Content-type":"application/json"
+    const navigate = useNavigate()
+    const [name, setName] = useState("")
+    const [email, setEmail] = useState("")
+    const [password, setPassword] = useState("")
+    const postData = () => {
+        console.log(name, email, password)
+        fetch("/register", {
+            method: "POST",
+            headers: {
+                "Content-type": "application/json"
             },
             body: JSON.stringify({
-                name,email,password
+                name, email, password
             })
-        }).then(res=>res.json())
-        .then(data=>{
-           if(data.error){
-            M.toast({html: data.error,classes:"toast-error"})
-           }
-           else{
-            M.toast({html:data.success,classes:"toast-success"})
-            navigate("/signin");
-           }
-        })
+        }).then(res => res.json())
+            .then(data => {
+                if (data.error) {
+                    M.toast({ html: data.error, classes: "toast-error" })
+                }
+                else {
+                    M.toast({ html: data.success, classes: "toast-success" })
+                    navigate("/signin")
+                }
+            })
     }
 
-  return (
-    <>  
-            
-        <div className='signup'>
-        <h2>Create new account</h2>
-        <input type="text" size={40} value={name} onChange={(e)=>{setName(e.target.value)}} placeholder='name'/><br/>
-        <input type="email" size={40} value={email} onChange={(e)=>{setEmail(e.target.value)}} placeholder='email'/><br/>
-        <input type="password" size={40} value={password} onChange={(e)=>{setPassword(e.target.value)}} placeholder='password'/><br/>
-        <button onClick={()=>postData()}>Sign up</button>
-        <br/>
-        <Link to="/signIn">Already have an account ?</Link>
-        </div>
-       
-    </>
-  )
+    return (
+        <>
+            <div className='signup'>
+                <h2 className='signup__title'>Create new account</h2>
+                <div className='input-field'>
+                    <input className='signup__input' type="text" size={40} value={name} onChange={(e) => { setName(e.target.value) }} placeholder='name' />
+                </div>
+                <div className='input-field'>
+                    <input className='signup__input' type="email" size={40} value={email} onChange={(e) => { setEmail(e.target.value) }} placeholder='email' />
+                </div>
+                <div className='input-field'>
+                    <input className='signup__input' type="password" size={40} value={password} onChange={(e) => { setPassword(e.target.value) }} placeholder='password' />
+                </div>
+                <button className='signup__button' onClick={() => postData()}>Sign up</button>
+                <br />
+                <Link className='signup__link' to="/signIn">Already have an account ?</Link>
+            </div>
+        </>
+    )
 }
 
 export default Signup
+
